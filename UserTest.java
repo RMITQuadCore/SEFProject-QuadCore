10c349a07b2550eeda6fe806a844a6a9302a261c
juicool <s3801971@student.rmit.edu.au>
2020-05-14 02:22:55 +1000

    changes in user testcase and user's login method

--- UserTest.java
+++ UserTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -7,10 +8,19 @@ import static org.junit.Assert.*;
 
 public class UserTest {
     User u = new User();
-    User u1 = new User("st100", "juilee", "kulkarni", "juileekul", "juileekul", "123rty", "rmit"); // already signed up
-    // user
-    String[] login = { "juileekul", "123rty" }; // dummy values for login
-    String[] signup = { "st102", "jk", "kul", "jkul", "jkul", "123rty", "monash" }; // dummy values for signup
+    User u1 = new Student("ST101", "abc1", "abc1", "abc1", "abc1", "abc4", "abc1", 2.8, 1, 'm', '0');
+
+    String[] login = { "abc1", "abc4" }; // dummy values for login
+    String[] signup = { "jk", "kul", "rmit", "jkul", "jkul", "jkul" }; // dummy values for signup
+
+    @Before
+    public void setUp() throws Exception {
+        Project pr = new Project();
+
+        Project p1 = new Project("CL101", "PROJ201", "xyz1", "xyz1", null, 7);
+        pr.proj.add(p1);
+    }
+
     @Test
     public void signup() {
         try {
@@ -20,9 +30,10 @@ public class UserTest {
             System.err.println(e.getMessage());
         }
         String[] input = new String[6];
-        input[0] = u.getId();
-        input[1] = u.getFirstName();
-        input[2] = u.getLastName();
+       // input[0] = u.getId();
+        input[0] = u.getFirstName();
+        input[1] = u.getLastName();
+        input[2] = u.getOrg();
         input[3] = u.getEmailID();
         input[4] = u.getUserName();
         input[5] = u.getPassword();
@@ -59,8 +70,9 @@ public class UserTest {
 
                 check++;
             }
-            assertEquals(2, check);
+
 
         }
+        assertEquals(2, check);
     }
 }
