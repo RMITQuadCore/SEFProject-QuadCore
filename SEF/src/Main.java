28cdfab70aada16077c98048eeb4f92464f3fe7c
Gajsa <gajsa.shekhawat@gmail.com>
2020-05-20 01:58:18 +1000

    Removing .idea files

--- SEF/src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    private static String userFileName = "Users.txt";
-    public static void main(String[] args) {
-
-        User user = new User();
-        user.mainMenu();
-    }
-}
